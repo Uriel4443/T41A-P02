4ab3c1fe570e378c4c10e88a9697d72e5d7f413d
Uriel4443 <182829@upslp.edu.mx>
2025-09-08 19:20:16 -0600

    Update 03_query_faltas.sql

--- 03_query_faltas.sql
+++ 03_query_faltas.sql
@@ -1,6 +1,3 @@
-    SELECT g.periodo, g.seccion, g.nombre_grupo,
-           COUNT(*) FILTER (WHERE a.presente = FALSE) AS total_faltas
-    FROM asistencia a
-    JOIN grupos g ON a.periodo = g.periodo AND a.seccion = g.seccion
-    GROUP BY g.periodo, g.seccion, g.nombre_grupo
-    ORDER BY g.seccion;
+    SELECT g.periodo, g.clave_materia , COUNT(*) FILTER (WHERE a.presente = FALSE) AS total_faltas
+    FROM asistencia a JOIN grupo g ON a.clave_materia = g.clave_materia
+    GROUP BY g.periodo,g.clave_materia, g.materia ORDER BY g.materia;
